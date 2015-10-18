9a692b5b307217098feaab0e1f28562438f71bee
Don Johnson <copyleftdev@gmail.com>
2015-10-17 18:23:52 -0700

    added  tasks and a list  arrray to  integrate over tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,26 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
 
 
 //TASKS
 
-gulp.task('default', function (){
-  console.log('Hello World');
+// uglify
+
+gulp.task('scripts', function(){
+
+    // Minify gneral js
+    gulp.src('js/**/*.js')
+      .pipe(uglify())
+      .pipe(gulp.dest('build/js'));
+
+    // Minify vendor js
+    gulp.src('js/vendor/*.js')
+      .pipe(uglify())
+      .pipe(gulp.dest('build/vendor'));
 });
+
+gulp.task('styles', function(){
+  console.log('styles task executed.')
+});
+
+gulp.task('default',['scripts','styles']);
